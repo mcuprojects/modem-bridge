3a5510745e065cc21a14d248383a36b32cd62e8d
Roman Pletnev <pletnevroman95@gmail.com>
2021-01-13 01:47:02 +0300

    Implement some commands

--- bridge.py
+++ bridge.py
@@ -14,6 +14,23 @@ logger.level = logging.DEBUG
 # 2 M
 LOG_SIZE = 2 * 1024 * 1024
 
+def parse_sxrat(cmd):
+    try:
+        cmd_str = cmd.decode("utf-8")
+        sxrat = cmd_str.replace("AT^SXRAT=", "").strip()
+        return sxrat
+    except UnicodeDecodeError:
+        return "0,0"
+
+def parse_ceer(resp):
+    try:
+        resp_str = resp.decode("utf-8")
+        ceer = resp_str.replace("+CEER:", "").replace("OK", "").strip()
+        return ceer
+    except UnicodeDecodeError:
+        return ""
+
+
 class ModemBridge():
     def __init__(self, src_port, src_baud, dst_port, dst_baud):
         self.src_serial = serial.Serial()
@@ -25,37 +42,101 @@ class ModemBridge():
         self.dst_serial.port = dst_port
         self.dst_serial.timeout = 1
         self.is_running = False
-        self.discon_lock = threading.Lock()
+        #self.discon_lock = threading.Lock()
     
-    def rxtx(self, rx, tx):
+    def loop(self, src, dst):
         try:
+            sxrat = "0,0"
+
             while self.is_running:
-                while rx.in_waiting > 0:
-                    data = rx.read(rx.in_waiting)
-                    tx.write(data)
+                # receive command
+                cmd = bytes()
+                while not cmd.endswith(b"\r\n"):
+                    # read next byte(s)
+                    if src.in_waiting > 0:
+                        cmd += src.read(src.in_waiting)
+                    else:
+                        time.sleep(0.1)
+                logger.debug("Read command [{}]: {}".format(self.src_serial.port, cmd))
+                cmd = cmd.replace(b"at", b"AT")
+                
+                # process command
+                ignore = False
+                send_cmd = cmd
+                if cmd.startswith(b"AT"):
+                    if cmd.startswith((b"AT+", b"ATI", b"ATV")):
+                        send_cmd = cmd
+                    elif cmd.startswith(b"AT^SCTM?"):
+                        send_cmd = b"AT#TEMPMON?"
+                    elif cmd.startswith(b"AT^SXRAT="):
+                        ignore = True
+                        sxrat = parse_sxrat(cmd)
+                    elif cmd.startswith(b"AT^SXRAT?"):
+                        ignore = True
+                    elif cmd.startswith(b"AT^SCFG="):
+                        ignore = True
+                    elif cmd.startswith(b"AT^SPOW="):
+                        ignore = True
+                         
+                if not ignore:
+                    logger.debug("Send command [{}]: {}".format(self.dst_serial.port, send_cmd))
+                    dst.write(send_cmd)
+
+                # receive response
+                resp = bytes()
+                if not ignore:
+                    while not resp.endswith((b"OK\r\n", b"ERROR\r\n")):
+                        # read next byte(s)
+                        if dst.in_waiting > 0:
+                            resp += dst.read(dst.in_waiting)
+                        else:
+                            time.sleep(0.1)
+                    logger.debug("Read response [{}]: {}".format(self.dst_serial.port, resp))
+                    
+                # process response
+                send_resp = resp
+                if ignore:
+                    send_resp = b"\r\nOK\r\n"
+                if cmd.startswith(b"AT^SXRAT?"):
+                    send_resp = ("\r\n^SXRAT: {}\r\n\r\nOK\r\n".format(sxrat)).encode("utf-8")
+                elif cmd.startswith(b"AT+CEER"):
+                    ceer = parse_ceer(resp)
+                    send_resp = ("\r\n+CEER: \"{}\"\r\n\r\nOK\r\n".format(ceer)).encode("utf-8")
+                
+                logger.debug("Send response [{}]: {}".format(self.src_serial.port, send_resp))
+                src.write(send_resp)
+
+                # sleep, repeat
                 time.sleep(0.1)
+    
         except serial.SerialException as e:
             logger.error("bridge port error: {}".format(e))
-
-        self.is_running = False
-        self.disconnect()
+        finally:
+            self.is_running = False
+            self.disconnect()
     
+    def convert_at_command(self, cmd=""):
+        if cmd.startswith("AT"):
+            if cmd.startswith("AT+") or cmd.startswith("ATI") or cmd.startswith("ATV"):
+                return cmd
+        else:
+            return cmd
+
     def connect(self):
         try:
-            logger.info("Starting bridge {}@{} <-> {}@{}".format(self.src_serial.port, self.src_serial.baudrate, self.dst_serial.port, self.dst_serial.baudrate))
+            logger.info("Starting bridge {}@{} -> {}@{}".format(self.src_serial.port, self.src_serial.baudrate, self.dst_serial.port, self.dst_serial.baudrate))
             self.src_serial.open()
             self.dst_serial.open()
+            self.src_serial.reset_input_buffer()
+            self.dst_serial.reset_input_buffer()
             # create lock file
             #if self.lockfile is not None:
             #    os.system("touch {}".format(self.lockfile))
             # start threads
             self.is_running = True
-            thread_a = threading.Thread(target=self.rxtx, args=(self.src_serial, self.dst_serial))
+            thread_a = threading.Thread(target=self.loop, args=(self.src_serial, self.dst_serial))
             thread_a.setDaemon(True)
             thread_a.start()
-            thread_b = threading.Thread(target=self.rxtx, args=(self.dst_serial, self.src_serial))
-            thread_b.setDaemon(True)
-            thread_b.start()
             return True
         except serial.SerialException:
             logger.error("Failed to open serial port")
@@ -63,7 +144,7 @@ class ModemBridge():
         pass
 
     def disconnect(self):
-        self.discon_lock.acquire()
+        #self.discon_lock.acquire()
         # delete lock file
         #if self.lockfile is not None:
         #    os.system("rm {}".format(self.lockfile))
@@ -73,7 +154,7 @@ class ModemBridge():
         if self.dst_serial.is_open:
             self.dst_serial.close()
             logger.info("Closed bridge port {}".format(self.dst_serial.port))
-        self.discon_lock.release()
+        #self.discon_lock.release()
 
 def main():
     # read config file
@@ -83,18 +164,18 @@ def main():
     dst_port = config['Destination']['Port']
     dst_baud = config['Destination']['Baudrate']
     
-    #stdout = True
-    #if 'StdOut' in config:
-    #    stdout = config['StdOut']
+    stdout = True
+    if 'StdOut' in config:
+        stdout = config['StdOut']
     # set up logger
     #os.makedirs(config['LogsFolder'], exist_ok=True)
-    #log_format = '%(asctime)s %(levelname)s: %(message)s'
-    #log_handlers = [RotatingFileHandler(config['LogsFolder'] + '/bridge.log', maxBytes=LOG_SIZE, backupCount=5)]
-    #if stdout:
-    #    log_handlers.append(logging.StreamHandler(sys.stdout))
-    #logging.basicConfig(format=log_format,
-    #                    level=logging.INFO,
-    #                    handlers=log_handlers)
+    log_format = '%(asctime)s %(levelname)s: %(message)s'
+    log_handlers = [RotatingFileHandler('bridge.log', maxBytes=LOG_SIZE, backupCount=5)]
+    if stdout:
+        log_handlers.append(logging.StreamHandler(sys.stdout))
+    logging.basicConfig(format=log_format,
+                        level=logging.INFO,
+                        handlers=log_handlers)
     logger.info("< < < Bridge service started > > >")
     global connected
     connected = True
